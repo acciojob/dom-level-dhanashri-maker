2becc49b18218b054198aa9c7e8e189c9f86e26d
dhanashri-maker <thedhanashrishinde@gmail.com>
2026-03-19 09:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function findDomLevel() {
+    let element = document.getElementById("level");
+    let count = 0;
+
+    // Traverse up to the root <html>
+    while (element) {
+        count++;
+        element = element.parentElement;
+    }
+
+    alert("The level of the element is: " + count);
+}
+
+// Run after page loads
+window.onload = findDomLevel;   
